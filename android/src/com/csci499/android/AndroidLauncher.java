4b21e990c8315ebff009228c2f48682dd064727c
Tyler McDaniel <bmcdanie@unca.edu>
2015-04-10 19:24:43 -0400

    Fixed and tested basic functionality on my Android device.

--- android/src/com/csci499/android/AndroidLauncher.java
+++ android/src/com/csci499/android/AndroidLauncher.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.csci499.TerrorGDXGame;
+import com.csci499.TestAdapter;
 import com.util.Platform;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -13,6 +14,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new TerrorGDXGame(Platform.ANDROID), config);
+		initialize(new TestAdapter(Platform.ANDROID), config);
 	}
 }
