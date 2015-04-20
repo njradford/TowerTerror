0e2fd43c2852f9d1985f6e96fe778189cb5b2207
Tyler McDaniel <bmcdanie@unca.edu>
2015-04-20 15:03:27 -0400

    Continue working in gameScreen to init board.

--- core/src/com/csci499/GameScreen.java
+++ core/src/com/csci499/GameScreen.java
@@ -22,6 +22,8 @@ import com.uwsoft.editor.renderer.Overlap2DStage;
 import com.uwsoft.editor.renderer.actor.CompositeItem;
 import com.uwsoft.editor.renderer.actor.IBaseItem;
 import com.uwsoft.editor.renderer.actor.ImageItem;
+import com.uwsoft.editor.renderer.actor.LabelItem;
+import com.uwsoft.editor.renderer.script.SimpleButtonScript;
 
 /**
  *
@@ -32,9 +34,18 @@ public class GameScreen extends AbstractScreen implements Screen {
     private GameStage stage;
     private CompositeItem root;
     private GameStateInterface gameState;
-    private CompositeItem[][] cardItems;
+
+    private ImageItem[][] cardItems;
+    private ImageItem[][] wallItems;
+    private ImageItem[][] castleItems;
     private ImageItem[] contextItems;
-    private CompositeItem progressItem;
+
+    private ImageItem nextButton;
+    private LabelItem monsterCounter;
+    private LabelItem[] playerNames;
+    private LabelItem[] playerScores;
+    private LabelItem otherPlayerLabel;
+
     private CompositeItem[] monsterItems;
     public GameScreen() {
         super();
@@ -49,26 +60,69 @@ public class GameScreen extends AbstractScreen implements Screen {
         } catch (NullPointerException e) {
             System.err.println("ERR -- GAMESCREEN -- () -- GAMESTATE NOT INITALIZED");
         }
-
+        //access modifiable view layer components
         root = stage.sceneLoader.getRoot();
-        contextItems = new ImageItem[] {(ImageItem)root.getItemById("orderDiscard"),
-                (ImageItem) root.getItemById("orderTrade"),
-                (ImageItem) root.getItemById("orderAttack")};
+        contextItems = new ImageItem[] {root.getImageById("orderDiscard"),
+                root.getImageById("orderTrade"),
+                root.getImageById("orderAttack")};
 
-        for (ImageItem context: contextItems) {
-        context.addAction(Actions.moveBy(-context.getWidth(), 0));
-        }
+        monsterCounter = root.getLabelById("monstersRemainingNum");
+
+        cardItems = new ImageItem[][] {{root.getImageById("playerCard0"), root.getImageById("playerCard1"),
+                root.getImageById("playerCard2"), root.getImageById("playerCard3"),root.getImageById("playerCard4"),
+                root.getImageById("playerCard5")}, {root.getImageById("otherCard0"), root.getImageById("otherCard1"),
+                root.getImageById("otherCard2"), root.getImageById("otherCard3"), root.getImageById("otherCard4"),
+                root.getImageById("otherCard5")}};
+
+        wallItems = new ImageItem[][] {{root.getImageById("wall_red1"), root.getImageById("wall_red2"),
+                root.getImageById("wall_green1"), root.getImageById("wall_green2"),root.getImageById("wall_blue1"),
+                root.getImageById("wall_blue2")}, {root.getImageById("wallRubble_red1"), root.getImageById("wallRubble_red2"),
+                root.getImageById("wallRubble_green1"), root.getImageById("wallRubble_green2"), root.getImageById("wallRubble_blue1"),
+                root.getImageById("wallRubble_blue2")}, {root.getImageById("wallCleared_red1"), root.getImageById("wallCleared_red2"),
+                root.getImageById("wallCleared_green1"), root.getImageById("wallCleared_green2"), root.getImageById("wallCleared_blue1"),
+                root.getImageById("wallCleared_green2")}};
+
+        castleItems = new ImageItem[][] {{root.getImageById("castle_red1"), root.getImageById("castle_red2"),
+                root.getImageById("castle_green1"), root.getImageById("castle_green2"),root.getImageById("castle_blue1"),
+                root.getImageById("castle_blue2")}, {root.getImageById("castleRubble_red1"), root.getImageById("castleRubble_red2"),
+                root.getImageById("castleRubble_green1"), root.getImageById("castleRubble_green2"), root.getImageById("castleRubble_blue1"),
+                root.getImageById("castleRubble_blue2")}, {root.getImageById("castleCleared_red1"), root.getImageById("castleCleared_red2"),
+                root.getImageById("castleCleared_green1"), root.getImageById("castleCleared_green2"), root.getImageById("castleCleared_blue1"),
+                root.getImageById("castleCleared_green2")}};
 
 
+        playerNames = new LabelItem[] {root.getLabelById("mainPlayersName"), root.getLabelById("otherPlayersName")};
 
+        playerScores = new LabelItem[] {root.getLabelById("playerScore"), root.getLabelById("otherScore")};
 
+        otherPlayerLabel = root.getLabelById("playerNumber");
 
+        nextButton = stage.sceneLoader.getRoot().getImageById("nextButton");
+        nextButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                nextButton();
+            }
+        });
+
+        initializeBoard();
 
         }
 
 
     public void initializeBoard() {
 
+        for (ImageItem context: contextItems) {
+            context.addAction(Actions.moveBy(-context.getWidth(), 0));
+            context.getColor().a=0; //make them invisible to start with, and off screen
+        }
+        monsterCounter.setText(String.valueOf(gameState.getNumMonsterTokens()));
+
+        playerNames[0].setText(gameState.getPlayerName(gameState.getCurrentPlayer()));
+        if (gameState.getPlayers() > 1) {
+            playerNames[1].setText(gameState.getPlayerName(gameState.getCurrentPlayer() + 1 % 6));
+        }
+
+        
         /*
             for (int i = 0; i < cardButtons.length; i++) {
             java.awt.Component[] comps = handPanels[i].getComponents();
@@ -105,6 +159,7 @@ public class GameScreen extends AbstractScreen implements Screen {
         }
 
          */
+
     }
 
     //draw loop function
@@ -152,5 +207,9 @@ public class GameScreen extends AbstractScreen implements Screen {
     @Override
     public Overlap2DStage getStage() {
         return stage;
+    }
+
+    private void nextButton() {
+        //button handler for next hand button
     }
 }
